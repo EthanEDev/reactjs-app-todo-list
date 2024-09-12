991929db568b977389617082fa41fa1d17981092
EthanEDev <ethan.ekhtiyar@gmail.com>
2024-09-12 14:24:28 +0330

    some comments added for reducer

--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,10 +1,12 @@
 export const tasksReducer = (state, action) => {
+  // Create a copy of the current state to avoid direct mutation
   const newState = { ...state };
   switch (action.type) {
+    // Update input field for adding new tasks
     case "UPDATE_INPUT_TEXT":
       newState.newTask = action.payload;
       break;
-
+    // Add a new task to the task list
     case "ADD_TASK":
       action.payload.preventDefault();
       newState.tasks.push({
@@ -15,30 +17,28 @@ export const tasksReducer = (state, action) => {
       newState.newTask = "";
       // action.payload.input.current.focus();
       break;
-
     case "EDIT_TASK":
-      newState.tasks[action.payload].text =
-        newState.tasks[action.payload].editedText;
+      // newState.tasks[action.payload].text =
+      //   newState.tasks[action.payload].editedText;
       break;
-
+    // Mark a task as done or undo it
     case "DONE_TASK":
       newState.tasks[action.payload].done =
         !newState.tasks[action.payload].done;
       break;
-
+    // Delete a task from the task list
     case "DELETE_TASK":
       newState.tasks.splice(action.payload, 1);
       break;
-
     default:
       break;
   }
-
+  // Store the updated task list in localStorage to persist data
   localStorage.setItem("copy-tasks", JSON.stringify(newState.tasks));
-
+  // Return the updated state
   return newState;
 };
-
+// Initial state for the app, loading tasks from localStorage or initializing an empty task list
 export const initState = {
   tasks: JSON.parse(localStorage.getItem("copy-tasks")) ?? [],
   newTask: "",
